0e0c3d63db29f729ee69491e5f485699187da3b8
Christian Bagaya <christianbagaya@Christians-MacBook-Pro.local>
2020-02-19 11:11:30 +0000

    whites

--- sfats/settings.py
+++ sfats/settings.py
@@ -47,6 +47,7 @@ AUTH_USER_MODEL = 'users.User'
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sfats.urls'
