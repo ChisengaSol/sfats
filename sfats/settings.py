3913c8455b29818731bd666fe14c7d342d32d2cc
Christian Bagaya <christianbagaya@Christians-MacBook-Pro.local>
2020-02-19 11:40:59 +0000

    whites to requirements

--- sfats/settings.py
+++ sfats/settings.py
@@ -47,7 +47,7 @@ AUTH_USER_MODEL = 'users.User'
 
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
